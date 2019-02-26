cba6191e3d59c9588c3d915c8e76b86b95bf8487
himanshu <himanshuagarwal272@gmail.com>
2019-02-26 17:49:05 +0530

    add the database orm and driver

--- main.go
+++ main.go
@@ -5,7 +5,47 @@ import (
     "log"
     "net/http"
     "github.com/gorilla/mux"
+    "github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
 )
+
+
+var db *gorm.DB
+
+func Get() *gorm.DB {
+	return db
+}
+
+func Connect(url string, maxIdleConnections, maxOpenConnections int) error {
+	var err error
+	db, err = gorm.Open("postgres", url)
+	if err != nil {
+		return err
+	}
+	db.DB()
+	err = db.DB().Ping()
+	if err != nil {
+		return err
+	}
+	db.LogMode(false)
+	db.DB().SetMaxIdleConns(maxIdleConnections)
+	db.DB().SetMaxOpenConns(maxOpenConnections)
+	db.SingularTable(false)
+	return nil
+}
+
+func Close() {
+	db.Close()
+}
+
+func init() {
+    url := "host=10.10.10.10 user=postgres dbname=sample_go_api password= port=5432 sslmode=disable"
+    err := Connect(url,10,10)
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("DB connection : Done")
+}
 
 func GetUser(w http.ResponseWriter, r *http.Request) {
     params := mux.Vars(r)
