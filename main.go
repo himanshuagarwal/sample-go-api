b2b61f0cfba2ed5b4b73bb9f80c0e022c70fcaae
himanshu <himanshuagarwal272@gmail.com>
2019-02-27 02:25:24 +0530

    refactor the code

--- main.go
+++ main.go
@@ -1,61 +1,60 @@
 package main
 
 import (
-    "encoding/json"
-    "log"
-    "net/http"
-    "github.com/gorilla/mux"
-    "github.com/jinzhu/gorm"
-	_ "github.com/lib/pq"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
+type User struct {
+	gorm.Model
+	Name  string
+	Email string
+}
 
 var db *gorm.DB
+var err error
 
-func Get() *gorm.DB {
-	return db
-}
-
-func Connect(url string, maxIdleConnections, maxOpenConnections int) error {
-	var err error
+func initDB() {
+	url := "host=localhost user=postgres dbname=sample_go_api password= port=5432 sslmode=disable"
 	db, err = gorm.Open("postgres", url)
-	if err != nil {
-		return err
-	}
-	db.DB()
-	err = db.DB().Ping()
-	if err != nil {
-		return err
-	}
-	db.LogMode(false)
-	db.DB().SetMaxIdleConns(maxIdleConnections)
-	db.DB().SetMaxOpenConns(maxOpenConnections)
-	db.SingularTable(false)
-	return nil
-}
-
-func Close() {
-	db.Close()
-}
-
-func init() {
-    url := "host=10.10.10.10 user=postgres dbname=sample_go_api password= port=5432 sslmode=disable"
-    err := Connect(url,10,10)
 	if err != nil {
 		panic(err)
 	}
-	log.Printf("DB connection : Done")
-}
 
-func GetUser(w http.ResponseWriter, r *http.Request) {
-    params := mux.Vars(r)
-    log.Printf("Getting User "+params["id"])
-    json.NewEncoder(w).Encode("test")
+	defer db.Close()
+
+	db.AutoMigrate(&User{})
 }
 
 func main() {
-    router := mux.NewRouter()
-    log.Printf("Starting web server at port 8000")
-    router.HandleFunc("/user/{id}", GetUser).Methods("GET")
-    log.Fatal(http.ListenAndServe(":8000", router))
+	//Intialize Router
+	router := mux.NewRouter()
+
+	log.Printf("Connecting to database")
+	initDB()
+
+	//Routes
+	router.HandleFunc("/users", GetUsers).Methods("GET")
+	router.HandleFunc("/user/{id}", GetUser).Methods("GET")
+	router.HandleFunc("/user/{id}", CreateUser).Methods("POST")
+	router.HandleFunc("/user/{id}", DeleteUser).Methods("DELETE")
+
+	//Starting Server
+	log.Printf("Starting web server at port 8000")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+}
+
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
 }
